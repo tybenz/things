68643545758475cba0dc2834b71ec009b1813ef6
Tyler Benziger <tabenziger@gmail.com>
2015-02-27 10:31:04 -0800

    CORS

--- app/server.js
+++ app/server.js
@@ -6,6 +6,21 @@ var multer = require( 'multer' );
 var app = express();
 var stateMachine = require( './state_machine' );
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use( allowCrossDomain );
+
 browserify.settings({
     insertGlobals: true,
     extensions: [ '.jsx' ],
